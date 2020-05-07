cfc69a67c45944d64aa82db7a4d104310299d657
glatan <glatan.edu@gmail.com>
2020-05-07 20:11:25 +0900

    yewを使ったWebアプリケーションとして再設計
    * textareaの更新時にハッシュ値が更新されない
    
    * ビルドの出力ファイル名を指定(ghash.{js,wasm},index.js,index.html}
    * ghashをライブラリとして利用する形にして、src/以下からghashのコードを全削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const WasmPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-const dist = path.resolve(__dirname, "dist");
+const distPath = path.resolve(__dirname, "dist");
 
 module.exports = {
   entry: {
-    index: "./static/index.js"
+    index: './static/index.js'
   },
   output: {
-    path: dist,
-    filename: "index.js",
-    chunkFilename: "[name].js"
+    path: distPath,
+    filename: 'index.js',
+    chunkFilename: "[name].js",
+    webassemblyModuleFilename: "ghash.wasm"
   },
   devServer: {
-    contentBase: dist,
+    contentBase: distPath,
+    port: 8000
   },
   plugins: [
     new CopyPlugin([
       path.resolve(__dirname, "static")
     ]),
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-      outName: "ghash-wasm"
-    }),
+    new WasmPlugin({
+      crateDirectory: ".",
+      extraArgs: "--no-typescript"
+    })
   ],
 };
