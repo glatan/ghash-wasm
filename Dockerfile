d246d8a67fb619aa7d94dab46473914202c9ff0a
glatan <glatan@protonmail.com>
2022-02-27 17:25:22 +0900

    Use Trunk

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /workdir
 COPY init.sh .
 
 RUN \
-    curl -sL https://rpm.nodesource.com/setup_16.x | bash - && \
     yum update -y && \
     yum install -y \
         gcc gzip nodejs tar && \
--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 
-export WASMPACK_VERSION='0.10.2'
-export CARGO_HOME='/cargo'
+TRUNK_VERSION='0.14.0'
+readonly TRUNK_VERSION
+CARGO_HOME='/cargo'
+# shellcheck disable=SC2034
+readonly CARGO_HOME
 
 # setup stable version of Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init.sh
@@ -11,6 +14,6 @@ sh rustup-init.sh -y \
     -t wasm32-unknown-unknown
 mv -f /cargo/bin/* /usr/bin
 
-# install wasm-pack
-curl -L "https://github.com/rustwasm/wasm-pack/releases/download/v${WASMPACK_VERSION}/wasm-pack-v${WASMPACK_VERSION}-x86_64-unknown-linux-musl.tar.gz" | tar xz
-mv -f wasm-pack-v${WASMPACK_VERSION}-x86_64-unknown-linux-musl/wasm-pack /usr/bin
+# install trunk
+curl -L "https://github.com/thedodd/trunk/releases/download/v${TRUNK_VERSION}/trunk-v${TRUNK_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | tar xz
+mv -f trunk-v${TRUNK_VERSION}-x86_64-unknown-linux-musl/wasm-pack /usr/bin
